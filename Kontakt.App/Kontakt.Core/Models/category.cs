d79d6dd9f40104f2b232fdf653b74f3ad686e942
İlkin <ilkinhd@code.edu.az>
2023-08-27 23:28:16 +0400

    Category table is created

---
+++ Kontakt.App/Kontakt.Core/Models/category.cs
@@ -0,0 +1,12 @@
+﻿namespace Kontakt.App.Models
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string Name { get; set; }
+        public int? ParentCategoryId { get; set; }
+        public Category ParentCategory { get; set; }
+        public List<Category> Subcategories { get; set; }
+        public List<Product> Products { get; set; }
+    }
+}
