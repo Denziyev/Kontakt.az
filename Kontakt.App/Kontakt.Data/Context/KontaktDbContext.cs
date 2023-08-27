bf98aecf1e999a8a7d13d0b4408e4127aeee62c4
İlkin <ilkinhd@code.edu.az>
2023-08-27 23:29:10 +0400

    Kontakt.Data and Kontakt.Service are added

---
+++ Kontakt.App/Kontakt.Data/Context/KontaktDbContext.cs
@@ -0,0 +1,16 @@
+﻿
+
+using Kontakt.App.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Kontakt.App.Context
+{
+    public class KontaktDbContext : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public KontaktDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
---
+++ Kontakt.App/Kontakt.Service/Extentions/FileUpload.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Book.Service.Extentions
+{
+    public static class FileUpload
+    {
+        public static bool isImage(this IFormFile file)
+        {
+            return file.ContentType.Contains("image");
+        }
+
+        public static bool isSizeOk(this IFormFile file,int mb)
+        {
+            return file.Length / 1024 / 1024 <= mb;
+        }
+
+        public static string CreateImage(this IFormFile file, string root, string path)
+        {
+            string FileName = Guid.NewGuid().ToString() + file.FileName;
+            string FullPath = Path.Combine(root, path, FileName);
+
+            using (FileStream strem = new FileStream(FullPath, FileMode.Create))
+            {
+                file.CopyTo(strem);
+            }
+            return FileName;
+        }
+    }
+}
