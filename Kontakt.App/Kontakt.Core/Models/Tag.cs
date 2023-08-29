36a2c50a992e0e1dc8aa7e421f2c96a083da2bdd
İlkin <ilkinhd@code.edu.az>
2023-08-30 02:30:09 +0400

    Tag Model is added

---
+++ Kontakt.App/Kontakt.Core/Models/Tag.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kontakt.Core.Models
+{
+    public class Tag : BaseModel
+    {
+        public string Name { get; set; }
+        public List<ProductTag>? ProductTags { get; set; }
+
+    }
+}
