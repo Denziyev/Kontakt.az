7bd1027f5ca8e3093f6abc2ffb80bd2bdc51efb6
İlkin <ilkinhd@code.edu.az>
2023-08-30 02:29:23 +0400

    Discount model updated

--- Kontakt.App/Kontakt.Core/Models/Discount.cs
+++ Kontakt.App/Kontakt.Core/Models/Discount.cs
@@ -1,6 +1,8 @@
 ﻿using Kontakt.App.Models;
+using Microsoft.AspNetCore.Http;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,11 +12,33 @@ namespace Kontakt.Core.Models
     public class Discount:BaseModel
     {
         public string Name { get; set; }
-        public List<Product> Products { get; set; } 
-        public DateTime StartDate { get; set; }
+        public DateTime StartDate { get; set; } 
         public DateTime EndDate { get; set; }
-        public string Images { get; set; }
-        public Category Category { get; set; }
+        public List<DiscountCategory>? DiscountCategories { get; set; }
+
+    }
+
+    public class DiscountCategory : BaseModel
+    {
+        public List<DiscountImage>? Images { get; set; }
+        public Category? Category { get; set; }
         public int CategoryId { get; set; }
+
+        public List<DiscountofProduct>? DiscountofProducts { get; set; }
+
+        public Discount? Discount { get; set; }
+        public int DiscountId { get; set; }
+
+        [NotMapped]
+        public List<IFormFile>? FormFiles { get; set; }
+    }
+
+    public class DiscountofProduct : BaseModel
+    {
+        public List<Product>? Products { get; set; }
+        public double Percent { get; set; }
+
+        public DiscountCategory DiscountCategory { get; set; }
+        public int DiscountCategoryId { get; set; }
     }
 }
