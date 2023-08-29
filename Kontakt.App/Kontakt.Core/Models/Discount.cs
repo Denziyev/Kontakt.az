e774d1c7c74f4f6e088cf5393c1276c561de339f
İlkin <ilkinhd@code.edu.az>
2023-08-29 23:57:16 +0400

    Discount models are added

---
+++ Kontakt.App/Kontakt.Core/Models/Discount.cs
@@ -0,0 +1,20 @@
+﻿using Kontakt.App.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kontakt.Core.Models
+{
+    public class Discount:BaseModel
+    {
+        public string Name { get; set; }
+        public List<Product> Products { get; set; } 
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string Images { get; set; }
+        public Category Category { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
--- Kontakt.App/Kontakt.Core/Models/product.cs
+++ Kontakt.App/Kontakt.Core/Models/product.cs
@@ -5,8 +5,11 @@ namespace Kontakt.App.Models
     public class Product:BaseModel
     {
         public string Name { get; set; }
-        public decimal Price { get; set; }
+        public double Price { get; set; }
         public int CategoryId { get; set; }
         public Category? Category { get; set; }
+        public Dictionary<string, string> Properties { get; set; }
+        public int StockNumber { get; set; }
+        public string Image { get; set; }
     }
 }
