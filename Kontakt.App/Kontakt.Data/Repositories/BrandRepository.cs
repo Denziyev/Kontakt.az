89a811c55546c994b960c3a48928ecdcc0caf5e3
İlkin <ilkinhd@code.edu.az>
2023-09-04 23:58:38 +0400

    Brand Service added

---
+++ Kontakt.App/Kontakt.Data/Repositories/BrandRepository.cs
@@ -0,0 +1,20 @@
+﻿using Kontakt.App.Context;
+using Kontakt.Core.Models;
+using Kontakt.Core.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kontakt.Data.Repositories
+{
+    public class BrandRepository : Repository<Brand>, IBrandRepository
+    {
+        public BrandRepository(KontaktDbContext context) : base(context)
+        {
+
+        }
+      
+    }
+}
---
+++ Kontakt.App/Kontakt.Service/Services/Interfaces/IBrandService.cs
@@ -0,0 +1,20 @@
+﻿using Kontakt.App.Models;
+using Kontakt.Core.Models;
+using Kontakt.Service.Responses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kontakt.Service.Services.Interfaces
+{
+    public interface IBrandService
+    {
+        public Task<MvcResponse<Brand>> CreateAsync(Category category);
+        public Task<MvcResponse<Brand>> UpdateAsync(int id, Category category);
+        public Task<MvcResponse<Category>> GetAsync(int? id);
+        public Task<MvcResponse<List<Category>>> GetAllAsync();
+        public Task<MvcResponse<Category>> DeleteAsync(int id);
+    }
+}
