4c1ac2c6f57b1c7cf497dc7f8a269953f8c02dde
İlkin <ilkinhd@code.edu.az>
2023-09-12 23:58:45 +0400

    Basket Service addded

---
+++ Kontakt.App/Kontakt.Core/Models/BasketItem.cs
@@ -0,0 +1,19 @@
+﻿using Kontakt.App.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kontakt.Core.Models
+{
+    public class BasketItem:BaseModel
+    {
+        public int ProductId { get; set; }
+        public int BasketId { get; set; }
+        public Basket Basket { get; set; }  
+        public Product Product { get; set; }
+        public int ProductCount { get; set; }
+
+    }
+}
